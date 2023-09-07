4b6b055008102536b1c7f0ab0a0a9d8511cf2eb5
Michaël Petrov <smartptr@gmail.com>
2023-09-07 20:03:17 +0200

    enhanced stack check

--- src/ghit/stack_commands.py
+++ src/ghit/stack_commands.py
@@ -1,9 +1,8 @@
-from collections.abc import Iterator
-
 import pygit2 as git
 
+from . import gh, terminal
+from . import gh_formatting as ghf
 from . import styling as s
-from . import terminal
 from .args import Args
 from .common import connect, push_and_pr
 from .error import GhitError
@@ -11,59 +10,76 @@ from .gitools import last_commits
 from .stack import Stack
 
 
-def _check_stack(
-    repo: git.Repository, stack: Stack
-) -> Iterator[tuple[Stack, int]]:
-    if repo.is_empty:
-        return
-    depth = 0
-    for record in stack.rtraverse(False):
-        if record.depth < depth:
+def has_finished_pr(repo: git.Repository, gh: gh.GH, record: Stack):
+    prs = gh.get_prs(record.branch_name)
+    all_finished = all(pr.state in ['CLOSED', 'MERGED'] and repo.lookup_branch(record.branch_name) for pr in prs)
+    for pr in prs:
+        if pr.state in ['CLOSED', 'MERGED'] and repo.lookup_branch(record.branch_name):
+            terminal.stdout(
+                s.good('🗸 Found PR'),
+                ghf.pr_number_with_style(pr),
+                s.good('with head'),
+                s.emphasis(record.branch_name) + s.good('.'),
+            )
+            terminal.stdout(
+                ' ',
+                s.good('You may delete local branch with `') + 'git branch --delete',
+                s.emphasis(record.branch_name) + s.good('`.'),
+            )
+            terminal.stdout()
             break
-        parent = record.get_parent()
-        parent_ref = repo.references.get(f'refs/heads/{parent.branch_name}')
-        record_ref = repo.references.get(f'refs/heads/{record.branch_name}')
-        if not record_ref:
-            continue
-        a, _ = repo.ahead_behind(parent_ref.target, record_ref.target)
-        if a != 0:
-            yield (record, a)
-            depth = record.depth
+    return prs and all_finished
 
 
 def check(args: Args) -> None:
-    repo, stack, _ = connect(args)
-    for notsync in _check_stack(repo, stack):
-        record, a = notsync
+    repo, stack, gh = connect(args)
+    if repo.is_empty:
+        return
+
+    insync = True
+    for record in stack.traverse(False):
+        if has_finished_pr(repo, gh, record):
+            continue
+        parent = record.get_parent()
+        parent_ref = repo.references.get(f'refs/heads/{parent.branch_name}')
+        ref = repo.references.get(f'refs/heads/{record.branch_name}')
+        if not ref:
+            continue
+        a, b = repo.ahead_behind(parent_ref.target, ref.target)
+        if a or b:
+            insync = False
+
         terminal.stdout(
             s.warning('🗶'),
             s.emphasis(record.get_parent().branch_name),
             s.warning('is ahead of'),
             s.emphasis(record.branch_name),
-            s.warning('with:'),
-        )
-        parent_ref = repo.references.get(
-            f'refs/heads/{record.get_parent().branch_name}'
+            s.warning(f'with {a} commits:' if a != 1 else f'with {a} commit:'),
         )
 
         for commit in last_commits(repo, parent_ref.target, a):
-            terminal.stdout(
-                s.inactive(
-                    f'\t[{commit.short_id}] {commit.message.splitlines()[0]}'
-                )
-            )
+            terminal.stdout(s.inactive(f'\t[{commit.short_id}] {commit.message.splitlines()[0]}'))
 
         terminal.stdout(
-            '  Run `git rebase -i '
-            + record.get_parent().branch_name
-            + ' '
-            + record.branch_name
-            + '`.'
+            ' ',
+            s.warning('while'),
+            s.emphasis(record.branch_name),
+            s.warning((f'has {b} commits' if b != 1 else f'has {b} commit') + ' on top of'),
+            s.emphasis(record.get_parent().branch_name) + s.warning(':'),
         )
+        for commit in last_commits(repo, ref.target, b):
+            terminal.stdout(s.inactive(f'\t[{commit.short_id}] {commit.message.splitlines()[0]}'))
 
-    if not notsync:
-        terminal.stdout(s.good('🗸 The stack is in shape.'))
-    else:
+        terminal.stdout(
+            ' ',
+            s.warning('Run `') + 'git rebase -i --onto',
+            s.emphasis(record.get_parent().branch_name),
+            s.emphasis(record.branch_name) + s.warning(f'~{b}'),
+            s.emphasis(record.branch_name) + s.warning('`.'),
+        )
+        terminal.stdout()
+
+    if not insync:
         raise GhitError(s.warning('The stack is not in shape.'))
 
 
